39b917c1e5fb68a0f71b23ac5a37bb50b65178c9
andrearagao <business@andreribeiroaragao.com>
2023-06-13 23:58:28 +0100

    Implement 2 additional endpoints to support 3 price quotation methods

--- src/main/java/org/araragao/shopping/platform/api/OrderApi.java
+++ src/main/java/org/araragao/shopping/platform/api/OrderApi.java
@@ -3,19 +3,35 @@ package org.araragao.shopping.platform.api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.araragao.shopping.platform.api.dto.OrderDiscountedBestDto;
+import org.araragao.shopping.platform.api.dto.OrderDiscountedDto;
 import org.araragao.shopping.platform.api.dto.OrderDto;
 import org.araragao.shopping.platform.api.dto.OrderPriceDto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(name = "Orders", description = "Operations related to order process")
+@Tag(name = "Orders", description = "Operations related to order processing")
 @RequestMapping("/api/orders")
 public interface OrderApi {
 
-  @PostMapping("/best-price")
+  @PostMapping("/price")
   @RequestBody
   @Operation(
-      summary = "Get the best Order price based on active PolicyDiscount",
-      description = "Get the best `Order` price based on active `PolicyDiscount`.")
-  OrderPriceDto getMinOrderPrice(OrderDto orderDto);
+      summary = "Get the (standard) order price",
+      description = "Get the (standard) order price.")
+  OrderPriceDto getOrderPrice(OrderDto orderDto);
+
+  @PostMapping("/price/discounted")
+  @RequestBody
+  @Operation(
+      summary = "Get the discounted order price based on a specific PolicyDiscount",
+      description = "Get the discounted order price based on a specific `PolicyDiscount`.")
+  OrderPriceDto getDiscountedOrderPrice(OrderDiscountedDto orderDiscountedDto);
+
+  @PostMapping("/price/discounted/best")
+  @RequestBody
+  @Operation(
+      summary = "Get the best order price based on active related PolicyDiscount",
+      description = "Get the best order price based on active related `PolicyDiscount`.")
+  OrderPriceDto getBestDiscountedOrderPrice(OrderDiscountedBestDto orderDiscountedBestDto);
 }
